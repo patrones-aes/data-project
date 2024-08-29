267ff069c8fe1304c933de35cd99360fdc33b7df
David Ramirez <ramirezmonroyd@gmail.com>
2024-08-29 15:15:18 -0500

    update initial sql script

--- data/repository-db-schema/liquibase/changelog/0.0.1-initial/0001-initial.sql
+++ data/repository-db-schema/liquibase/changelog/0.0.1-initial/0001-initial.sql
@@ -12,46 +12,15 @@ DROP SCHEMA IF EXISTS "meta" CASCADE;
 CREATE SCHEMA IF NOT EXISTS "meta";
 
 --
--- Name: sources; Type: TABLE; Schema: meta; Owner: -
---
-CREATE TABLE "meta"."sources" (
-    "code" SERIAL,
-    "description" CHARACTER VARYING NOT NULL,
-    "type" INT NOT NULL,
-    "state" CHARACTER VARYING NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
-    CONSTRAINT pk_source PRIMARY KEY ("code")
-);
-
---
--- Name: source_types; Type: TABLE; Schema: meta; Owner: -
---
-CREATE TABLE "meta"."source_types" (
-    "code" SERIAL,
-    "description" CHARACTER VARYING NOT NULL,
-    CONSTRAINT pk_source_type PRIMARY KEY ("code")
-);
-
---
 -- Name: data_bases; Type: TABLE; Schema: meta; Owner: -
 --
-CREATE TABLE "meta"."data_bases" (
+CREATE TABLE "meta"."databases" (
     "code" SERIAL,
     "name" CHARACTER VARYING NOT NULL,
     "ip" CHARACTER VARYING NOT NULL,
     "version" CHARACTER VARYING NOT NULL,
-    "engine" INT NOT NULL,
-    "source" INT NOT NULL,
-    CONSTRAINT pk_data_base PRIMARY KEY ("code")
-);
-
---
--- Name: data_base_engines; Type: TABLE; Schema: meta; Owner: -
---
-CREATE TABLE "meta"."data_base_engines" (
-    "code" SERIAL,
-    "name" CHARACTER VARYING,
-    CONSTRAINT pk_data_base_engine PRIMARY KEY ("code")
+    "port" CHARACTER VARYING NOT NULL,
+    CONSTRAINT pk_database PRIMARY KEY ("code")
 );
 
 --
@@ -60,8 +29,8 @@ CREATE TABLE "meta"."data_base_engines" (
 CREATE TABLE "meta"."tables" (
     "code" SERIAL,
     "name" CHARACTER VARYING NOT NULL,
-    "description" CHARACTER VARYING NOT NULL,
-    "data_base" INT NOT NULL,
+    "database" CHARACTER VARYING NOT NULL,
+    "entity" CHARACTER VARYING NOT NULL,
     CONSTRAINT pk_table PRIMARY KEY ("code")
 );
 
@@ -71,18 +40,12 @@ CREATE TABLE "meta"."tables" (
 CREATE TABLE "meta"."columns" (
     "code" SERIAL,
     "name" CHARACTER VARYING NOT NULL,
-    "type" INT NOT NULL,
-    "table" INT NOT NULL,
+    "type" CHARACTER VARYING NOT NULL,
+    "table" CHARACTER VARYING NOT NULL,
+    "key" CHARACTER VARYING,
+    "length" INT,
+    "entity_attribute" CHARACTER VARYING NOT NULL,
     CONSTRAINT pk_column PRIMARY KEY ("code")
-);
-
---
--- Name: column_types; Type: TABLE; Schema: meta; Owner: -
---
-CREATE TABLE "meta"."column_types" (
-    "code" SERIAL,
-    "description" CHARACTER VARYING NOT NULL,
-    CONSTRAINT pk_column_type PRIMARY KEY ("code")
 );
 
 --
@@ -92,19 +55,7 @@ CREATE TABLE "meta"."entities" (
     "code" SERIAL,
     "name" CHARACTER VARYING NOT NULL,
     "description" CHARACTER VARYING NOT NULL,
-    "table" INT NOT NULL,
     CONSTRAINT pk_entity PRIMARY KEY ("code")
-);
-
---
--- Name: domain_rules; Type: TABLE; Schema: meta; Owner: -
---
-CREATE TABLE "meta"."domain_rules" (
-    "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
-    "rule" CHARACTER VARYING NOT NULL,
-    "entity" INT NOT NULL,
-    CONSTRAINT pk_domain_rule PRIMARY KEY ("code")
 );
 
 --
@@ -112,52 +63,8 @@ CREATE TABLE "meta"."domain_rules" (
 --
 CREATE TABLE "meta"."entity_attributes" (
     "code" SERIAL,
+    "name" CHARACTER VARYING NOT NULL,
     "description" CHARACTER VARYING NOT NULL,
-    "entity" INT NOT NULL,
+    "entity" CHARACTER VARYING NOT NULL,
     CONSTRAINT pk_entity_attributes PRIMARY KEY ("code")
 );
-
-ALTER TABLE
-    "meta"."sources"
-ADD
-    CONSTRAINT "fk_source_type" FOREIGN KEY ("type") REFERENCES "meta"."source_types"("code") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE
-    "meta"."data_bases"
-ADD
-    CONSTRAINT "fk_engine" FOREIGN KEY ("engine") REFERENCES "meta"."data_base_engines"("code") ON DELETE NO ACTION ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."data_bases"
-ADD
-    CONSTRAINT "fk_source" FOREIGN KEY ("source") REFERENCES "meta"."sources"("code") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-ALTER TABLE
-    "meta"."tables"
-ADD
-    CONSTRAINT "fk_data_base" FOREIGN KEY ("data_base") REFERENCES "meta"."data_bases"("code") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."columns"
-ADD
-    CONSTRAINT "fk_column_type" FOREIGN KEY ("type") REFERENCES "meta"."column_types"("code") ON DELETE NO ACTION ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."columns"
-ADD
-    CONSTRAINT "fk_tables" FOREIGN KEY ("table") REFERENCES "meta"."tables"("code") ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."entities"
-ADD
-    CONSTRAINT "fk_table" FOREIGN KEY ("table") REFERENCES "meta"."tables"("code") ON DELETE NO ACTION ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."domain_rules"
-ADD
-    CONSTRAINT "fk_entity" FOREIGN KEY ("entity") REFERENCES "meta"."entities"("code") ON DELETE NO ACTION ON UPDATE CASCADE;
-
-ALTER TABLE
-    "meta"."entity_attributes"
-ADD
-    CONSTRAINT "fk_entity" FOREIGN KEY ("entity") REFERENCES "meta"."entities"("code") ON DELETE NO ACTION ON UPDATE CASCADE
