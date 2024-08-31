5baad0723f85953a99f1da5d6fae5e30341a836a
David Ramirez <ramirezmonroyd@gmail.com>
2024-08-31 06:13:25 -0500

    create final version of the model

--- data/repository-db-schema/liquibase/changelog/0.0.1-initial/0001-initial.sql
+++ data/repository-db-schema/liquibase/changelog/0.0.1-initial/0001-initial.sql
@@ -12,11 +12,11 @@ DROP SCHEMA IF EXISTS "meta" CASCADE;
 CREATE SCHEMA IF NOT EXISTS "meta";
 
 --
--- Name: data_bases; Type: TABLE; Schema: meta; Owner: -
+-- Name: databases; Type: TABLE; Schema: meta; Owner: -
 --
 CREATE TABLE "meta"."databases" (
     "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
+    "name" CHARACTER VARYING UNIQUE NOT NULL,
     "ip" CHARACTER VARYING NOT NULL,
     "version" CHARACTER VARYING NOT NULL,
     "port" CHARACTER VARYING NOT NULL,
@@ -28,34 +28,67 @@ CREATE TABLE "meta"."databases" (
 --
 CREATE TABLE "meta"."tables" (
     "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
-    "database" CHARACTER VARYING NOT NULL,
-    "entity" CHARACTER VARYING NOT NULL,
+    "name" CHARACTER VARYING UNIQUE NOT NULL,
     CONSTRAINT pk_table PRIMARY KEY ("code")
 );
+
+--
+-- Name: database_tables; Type: TABLE; Schema: meta; Owner: -
+--
+CREATE TABLE "meta"."database_tables" (
+    "code" SERIAL,
+    "database_code" INT NOT NULL,
+    "table_code" INT NOT NULL,
+    CONSTRAINT pk_database_table PRIMARY KEY ("code"),
+    CONSTRAINT fk_database FOREIGN KEY ("database_code") REFERENCES "meta"."databases"("code"),
+    CONSTRAINT fk_table FOREIGN KEY ("table_code") REFERENCES "meta"."tables"("code")
+
+ );
 
 --
 -- Name: columns; Type: TABLE; Schema: meta; Owner: -
 --
 CREATE TABLE "meta"."columns" (
     "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
-    "type" CHARACTER VARYING NOT NULL,
-    "table" CHARACTER VARYING NOT NULL,
-    "key" CHARACTER VARYING,
-    "length" INT,
-    "entity_attribute" CHARACTER VARYING NOT NULL,
+    "name" CHARACTER VARYING UNIQUE NOT NULL,
     CONSTRAINT pk_column PRIMARY KEY ("code")
 );
+
+--
+-- Name: table_columns; Type: TABLE; Schema: meta; Owner: -
+--
+CREATE TABLE "meta"."table_columns" (
+    "code" SERIAL,
+    "type" CHARACTER VARYING NOT NULL,
+    "key" CHARACTER VARYING,
+    "length" INT,
+    "database_table_code" INT NOT NULL,
+    "column_code" INT NOT NULL,
+    CONSTRAINT pk_table_column PRIMARY KEY ("code"),
+    CONSTRAINT fk_database_table FOREIGN KEY ("database_table_code") REFERENCES "meta"."database_tables"("code"),
+    CONSTRAINT fk_column FOREIGN KEY ("column_code") REFERENCES "meta"."columns"("code")
+ );
 
 --
 -- Name: entities; Type: TABLE; Schema: meta; Owner: -
 --
 CREATE TABLE "meta"."entities" (
     "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
+    "name" CHARACTER VARYING UNIQUE NOT NULL,
     "description" CHARACTER VARYING NOT NULL,
     CONSTRAINT pk_entity PRIMARY KEY ("code")
+);
+
+--
+-- Name: entity_tables; Type: TABLE; Schema: meta; Owner: -
+--
+CREATE TABLE "meta"."entity_tables" (
+    "code" SERIAL,
+    "entity_code" INT NOT NULL,
+    "database_table_code" INT NOT NULL,
+    CONSTRAINT pk_entity_table PRIMARY KEY ("code"),
+    CONSTRAINT fk_entity FOREIGN KEY ("entity_code") REFERENCES "meta"."entities"("code"),
+    CONSTRAINT fk_database_table FOREIGN KEY ("database_table_code") REFERENCES "meta"."database_tables"("code")
 );
 
 --
@@ -63,8 +96,31 @@ CREATE TABLE "meta"."entities" (
 --
 CREATE TABLE "meta"."entity_attributes" (
     "code" SERIAL,
-    "name" CHARACTER VARYING NOT NULL,
+    "name" CHARACTER VARYING UNIQUE NOT NULL,
     "description" CHARACTER VARYING NOT NULL,
-    "entity" CHARACTER VARYING NOT NULL,
     CONSTRAINT pk_entity_attributes PRIMARY KEY ("code")
 );
+
+--
+-- Name: entity_entity_attributes; Type: TABLE; Schema: meta; Owner: -
+--
+CREATE TABLE "meta"."entity_entity_attributes" (
+    "code" SERIAL,
+    "entity_code" INT NOT NULL,
+    "entity_attribute_code" INT NOT NULL,
+    CONSTRAINT pk_entity_entity_attribute PRIMARY KEY ("code"),
+    CONSTRAINT fk_entity FOREIGN KEY ("entity_code") REFERENCES "meta"."entities"("code"),
+    CONSTRAINT fk_entity_attribute FOREIGN KEY ("entity_attribute_code") REFERENCES "meta"."entity_attributes"("code")
+ );
+
+--
+-- Name: entity_attributes_columns; Type: TABLE; Schema: meta; Owner: -
+--
+CREATE TABLE "meta"."entity_attributes_columns" (
+    "code" SERIAL,
+    "entity_entity_attribute_code" INT NOT NULL,
+    "table_column_code" INT NOT NULL,
+    CONSTRAINT pk_entity_attribute_column PRIMARY KEY ("code"),
+    CONSTRAINT fk_entity_entity_attribute FOREIGN KEY ("entity_entity_attribute_code") REFERENCES "meta"."entity_entity_attributes"("code"),
+    CONSTRAINT fk_table_column FOREIGN KEY ("table_column_code") REFERENCES "meta"."table_columns"("code")
+ );
